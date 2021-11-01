686efe0ff450aa92b67195f91dab2a682e5440bc
maddesso <mark@locus.sh>
2021-11-01 17:13:43 -0400

    changed port

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ const axios = require('axios');
 
 const app = express();
 // const port = 3000;
-const port = process.env.PORT || 80
+const port = process.env.PORT || 80;
 // const client = require("./client");
 
 import { CarrierConnector } from "./carriers/CarrierConnector";
